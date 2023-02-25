62b6cda5881f890e653d3b8997901d798a8cd24a
mario <marioworker@163.com>
2023-02-25 14:14:06 +0800

    test

---
+++ .commitlintrc.js
@@ -0,0 +1,9 @@
+/*
+ * @Date: 2023-02-25 14:04:31
+ * @LastEditors: mario marioworker@163.com
+ * @LastEditTime: 2023-02-25 14:12:31
+ * @Description: Do not edit
+ */
+module.exports = {
+  extends: ['gitmoji'],
+}
